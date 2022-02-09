597540513956d1c811124e7e30f365512b7204ba
B-Litty <bradlitman94@gmail.com>
2022-02-08 17:28:35 -0700

    cart state and functionality created

--- client/src/utils/GlobalState.js
+++ client/src/utils/GlobalState.js
@@ -5,11 +5,13 @@ const StoreContext = createContext();
 const { Provider } = StoreContext;
 
 const StoreProvider = ({ value = [], ...props }) => {
-    
+
     const [state, dispatch] = useProductReducer({
         products: [],
+        cart: [],
+        cartOpen: false,
         categories: [],
-        currentCategory: '',
+        currentCategory: ''
     });
     // use this to confirm it works!
     console.log(state);
